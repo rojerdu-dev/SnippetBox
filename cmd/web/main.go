7ddaba0320dd98eb1b713a520c2cc5017cc86b1b
Rojer Du <rojerdu.dev@gmail.com>
2023-11-26 11:31:19 -0800

    Add static files to be served

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,12 +8,17 @@ import (
 func main() {
 	// ServeMux
 	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	const port = "8000"
-	log.Printf("starting server on: %s", port)
+	log.Printf("starting server on port: %s", port)
 
 	err := http.ListenAndServe(":"+port, mux)
 	log.Fatal(err)

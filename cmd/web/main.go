80cda3fd393d32db7f1592c8e73eb1be9e8304c3
Rojer Du <rojerdu.dev@gmail.com>
2023-12-04 16:22:44 -0800

    Add flag to set port address at runtime and structured logging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,25 +1,41 @@
 package main
 
 import (
-	"log"
+	"flag"
+	"log/slog"
 	"net/http"
+	"os"
 )
 
 func main() {
+	// address flag
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+
+	//logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	//logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		AddSource:   true,
+		Level:       slog.LevelInfo,
+		ReplaceAttr: nil,
+	}))
+
 	// ServeMux
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	const port = "8000"
-	log.Printf("starting server on port: %s", port)
+	//const port = "8080"
+	//log.Printf("starting server on port%s\n", *addr)
+	logger.Info("starting server", slog.Any("addr", ":4000"))
 
-	err := http.ListenAndServe(":"+port, mux)
-	log.Fatal(err)
+	err := http.ListenAndServe(*addr, mux)
+	//log.Fatal(err)
+	logger.Error(err.Error())
+	os.Exit(1)
 }

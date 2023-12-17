388c72a6ac54847fecf447aae24ccb3c5dad77b2
Rojer Du <rojerdu.dev@gmail.com>
2023-12-16 22:51:30 -0800

    add mysql driver and successfully tested connection to db

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	_ "github.com/go-sql-driver/mysql"
 	"log/slog"
 	"net/http"
 	"os"
@@ -14,10 +16,23 @@ type application struct {
 func main() {
 	// set address at runtime
 	addr := flag.String("addr", ":8080", "HTTP network address")
+	dsn := flag.String(
+		"dsn",
+		"root:mysql@tcp(127.0.0.1:3306)/snippetbox?parseTime=true",
+		"MySQL data source name")
 	flag.Parse()
 
 	// structured logging
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	// connection pool into separate openDB() function below
+	db, err := openDB(*dsn)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	defer db.Close()
 
 	app := &application{
 		logger: logger,
@@ -28,7 +43,22 @@ func main() {
 
 	// call new app.routes() method to get servemux containing our routes
 	// pass to http.ListenAndServe
-	err := http.ListenAndServe(*addr, app.routes())
+	err = http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+// openDB() function wraps sql.open() and
+// returns a sql.DB connection pool for a given DSN
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
 }

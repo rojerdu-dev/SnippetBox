3da9e32ba462d04e68e4b22410d6431c6b17023e
Rojer Du <rojerdu.dev@gmail.com>
2024-01-31 12:51:48 -0800

    initial commit - setup basic server

--- main.go
+++ main.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	const port = ":8000"
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	log.Printf("starting server on port%s ...", port)
+	err := http.ListenAndServe(port, mux)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("home page for project"))
+}

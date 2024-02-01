5b505b0c9eaaea70e2f9b727f36359388467027e
Rojer Du <rojerdu.dev@gmail.com>
2024-01-31 16:36:26 -0800

    add serve static css img js functionality

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,15 +7,17 @@ import (
 
 func main() {
 	const port = ":8000"
-
 	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Printf("starting server on port%s ...", port)
-	err := http.ListenAndServe(port, mux)
-	if err != nil {
+	if err := http.ListenAndServe(port, mux); err != nil {
 		log.Fatal(err)
 	}
 }

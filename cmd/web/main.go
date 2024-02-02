38056f93771de7861756f97736ed278f2cddb123
Rojer Du <rojerdu.dev@gmail.com>
2024-02-01 22:45:22 -0800

    fix db connection issue in docker-compose.yml

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
-	"log"
 	"log/slog"
 	"net/http"
 	"os"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -14,17 +17,41 @@ type application struct {
 
 func main() {
 	addr := flag.String("addr", ":8000", "HTTP network address")
+	dsn := flag.String("dsn", "web:password@tcp(localhost:3306)/snippetbox?parseTime=true", "MySQL data source name")
 	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
-	app := application{
+	db, err := openDB(*dsn)
+	if err != nil {
+		logger.Error("Failed to open database ...", "error", err)
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	app := &application{
 		logger: logger,
 	}
 
 	logger.Info("starting server", slog.String("addr", *addr))
 
-	if err := http.ListenAndServe(*addr, app.routes()); err != nil {
-		log.Fatal(err)
+	err = http.ListenAndServe(*addr, app.routes())
+	logger.Error(err.Error())
+	os.Exit(1)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
 	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	db.SetConnMaxLifetime(time.Minute * 3)
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
+
+	return db, nil
 }

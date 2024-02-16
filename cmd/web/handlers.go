ac08c3d426392e79db7851ff2c5e0c6d81d26d8f
Rojer Du <rojerdu.dev@gmail.com>
2024-02-16 00:08:25 -0800

    add template caching

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"net/http"
 	"strconv"
 )
@@ -19,26 +18,30 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	files := []string{
-		"./ui/html/base.html",
-		"./ui/html/partials/nav.html",
-		"./ui/html/pages/home.html",
-	}
-
-	ts, err := template.ParseFiles(files...)
-	if err != nil {
-		app.serveError(w, r, err)
-		return
-	}
-
-	data := templateData{
+	app.render(w, r, http.StatusOK, "home.html", templateData{
 		Snippets: snippets,
-	}
-
-	err = ts.ExecuteTemplate(w, "base", data)
-	if err != nil {
-		app.serveError(w, r, err)
-	}
+	})
+	//
+	//files := []string{
+	//	"./ui/html/base.html",
+	//	"./ui/html/partials/nav.html",
+	//	"./ui/html/pages/home.html",
+	//}
+	//
+	//ts, err := template.ParseFiles(files...)
+	//if err != nil {
+	//	app.serveError(w, r, err)
+	//	return
+	//}
+	//
+	//data := templateData{
+	//	Snippets: snippets,
+	//}
+	//
+	//err = ts.ExecuteTemplate(w, "base", data)
+	//if err != nil {
+	//	app.serveError(w, r, err)
+	//}
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
@@ -74,25 +77,29 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	files := []string{
-		"./ui/html/base.html",
-		"./ui/html/partials/nav.html",
-		"./ui/html/pages/view.html",
-	}
-
-	ts, err := template.ParseFiles(files...)
-	if err != nil {
-		app.serveError(w, r, err)
-		return
-	}
-
-	data := templateData{
+	app.render(w, r, http.StatusOK, "view.html", templateData{
 		Snippet: snippet,
-	}
-
-	err = ts.ExecuteTemplate(w, "base", data)
-	if err != nil {
-		app.serveError(w, r, err)
-		return
-	}
+	})
+	//
+	//files := []string{
+	//	"./ui/html/base.html",
+	//	"./ui/html/partials/nav.html",
+	//	"./ui/html/pages/view.html",
+	//}
+	//
+	//ts, err := template.ParseFiles(files...)
+	//if err != nil {
+	//	app.serveError(w, r, err)
+	//	return
+	//}
+	//
+	//data := templateData{
+	//	Snippet: snippet,
+	//}
+	//
+	//err = ts.ExecuteTemplate(w, "base", data)
+	//if err != nil {
+	//	app.serveError(w, r, err)
+	//	return
+	//}
 }
--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,8 +1,38 @@
 package main
 
-import "github.com/rojerdu-dev/SnippetBox/internal/models"
+import (
+	"github.com/rojerdu-dev/SnippetBox/internal/models"
+	"html/template"
+	"path/filepath"
+)
 
 type templateData struct {
 	Snippet  models.Snippet
 	Snippets []models.Snippet
+}
+
+func newTemplateCache() (map[string]*template.Template, error) {
+	cache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./ui/html/pages/*.html")
+	if err != nil {
+		return nil, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+		files := []string{
+			"./ui/html/base.html",
+			"./ui/html/partials/nav.html",
+			page,
+		}
+
+		ts, err := template.ParseFiles(files...)
+		if err != nil {
+			return nil, err
+		}
+
+		cache[name] = ts
+	}
+	return cache, nil
 }

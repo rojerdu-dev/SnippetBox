e97865c4ef651b02eba9f56cd38f0b93dd444856
Rojer Du <rojerdu.dev@gmail.com>
2024-02-26 23:44:56 -0800

    Add user logout

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -128,9 +128,18 @@ func (app *application) userLoginPost(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/snippet/create", http.StatusSeeOther)
 }
 
-// TODO - param r blanked out for now
-func (app *application) userLogoutPost(w http.ResponseWriter, _ *http.Request) {
-	fmt.Println(w, "Logout the user ...")
+func (app *application) userLogoutPost(w http.ResponseWriter, r *http.Request) {
+	err := app.sessionManager.RenewToken(r.Context())
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+
+	app.sessionManager.Remove(r.Context(), "authenticatedUserID")
+
+	app.sessionManager.Put(r.Context(), "flash", "You've been logged out successfully!")
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
